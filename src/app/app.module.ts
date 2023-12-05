a900160acc37524caee8bcf84dd5b064bc9efdba
Matias <matuemendez89@gmail.com>
2023-12-05 20:13:13 -0300

    feat(loki-titles): new component and fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LokiTitlesComponent } from './loki-titles/loki-titles.component';
 import { AnimatedLineComponent } from './animated-line/animated-line.component';
 import { AbilitiesComponent } from './abilities/abilities.component';
+import { MyInfoComponent } from './my-info/my-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LokiTitlesComponent,
     AnimatedLineComponent,
-    AbilitiesComponent
+    AbilitiesComponent,
+    MyInfoComponent
   ],
   imports: [
     BrowserModule,
