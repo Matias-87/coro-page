5dd12337f5afe87e364d5bf29ba9f94a5ef751cb
Matias <matuemendez89@gmail.com>
2024-04-17 16:08:57 -0300

    feat(contact.component): new email options and responsive desing

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  copied = false;
 
+  async copyEmail() {
+    try {
+      await navigator.clipboard.writeText('matuemendez89@gmail.com');
+      this.copied = true;
+    } catch (err) {
+      console.error('Error al copiar en portapapeles: ', err);
+      this.copied = false;
+    }
+  }
 }
